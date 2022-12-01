ea1ec89485bf7a4bf6f7a3e5e3d2bf08e90e3079
jellywiz <muzaffer.dler@gmail.com>
2022-12-01 13:37:55 +0300

    Refactor calculator functional component&use hooks

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,58 +1,51 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import { useState } from 'react';
 import './Calculator.css';
 import calculate from './logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+export default function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick = (e) => {
-    const result = calculate(this.state, e.target.textContent);
-    this.setState(result);
+  const handleClick = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="calculator">
-        <div className="calculator-show">{ next || total }</div>
-        <div className="calculator-container">
-          <div className="input-keys">
-            <div className="function-keys">
-              <button onClick={this.handleClick} type="button" className="key">AC</button>
-              <button onClick={this.handleClick} type="button" className="key">+/-</button>
-              <button onClick={this.handleClick} type="button" className="key">%</button>
-            </div>
-            <div className="digit-keys">
-              <button onClick={this.handleClick} type="button" className="key">7</button>
-              <button onClick={this.handleClick} type="button" className="key">8</button>
-              <button onClick={this.handleClick} type="button" className="key">9</button>
-              <button onClick={this.handleClick} type="button" className="key">4</button>
-              <button onClick={this.handleClick} type="button" className="key">5</button>
-              <button onClick={this.handleClick} type="button" className="key">6</button>
-              <button onClick={this.handleClick} type="button" className="key">1</button>
-              <button onClick={this.handleClick} type="button" className="key">2</button>
-              <button onClick={this.handleClick} type="button" className="key">3</button>
-              <button onClick={this.handleClick} type="button" className="key key0">0</button>
-              <button onClick={this.handleClick} type="button" className="key">.</button>
-            </div>
+  return (
+    <div className="calculator">
+      <div className="calculator-show">{ state.next || state.total }</div>
+      <div className="calculator-container">
+        <div className="input-keys">
+          <div className="function-keys">
+            <button onClick={handleClick} type="button" className="key">AC</button>
+            <button onClick={handleClick} type="button" className="key">+/-</button>
+            <button onClick={handleClick} type="button" className="key">%</button>
           </div>
-          <div className="operator-keys">
-            <button onClick={this.handleClick} type="button" className="key orange-key">÷</button>
-            <button onClick={this.handleClick} type="button" className="key orange-key">x</button>
-            <button onClick={this.handleClick} type="button" className="key orange-key">-</button>
-            <button onClick={this.handleClick} type="button" className="key orange-key">+</button>
-            <button onClick={this.handleClick} type="button" className="key orange-key">=</button>
+          <div className="digit-keys">
+            <button onClick={handleClick} type="button" className="key">7</button>
+            <button onClick={handleClick} type="button" className="key">8</button>
+            <button onClick={handleClick} type="button" className="key">9</button>
+            <button onClick={handleClick} type="button" className="key">4</button>
+            <button onClick={handleClick} type="button" className="key">5</button>
+            <button onClick={handleClick} type="button" className="key">6</button>
+            <button onClick={handleClick} type="button" className="key">1</button>
+            <button onClick={handleClick} type="button" className="key">2</button>
+            <button onClick={handleClick} type="button" className="key">3</button>
+            <button onClick={handleClick} type="button" className="key key0">0</button>
+            <button onClick={handleClick} type="button" className="key">.</button>
           </div>
         </div>
+        <div className="operator-keys">
+          <button onClick={handleClick} type="button" className="key orange-key">÷</button>
+          <button onClick={handleClick} type="button" className="key orange-key">x</button>
+          <button onClick={handleClick} type="button" className="key orange-key">-</button>
+          <button onClick={handleClick} type="button" className="key orange-key">+</button>
+          <button onClick={handleClick} type="button" className="key orange-key">=</button>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
