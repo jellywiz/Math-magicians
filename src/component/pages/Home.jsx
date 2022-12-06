dae62f8752e29a38987fc764a1db4ece019b759a
jellywiz <muzaffer.dler@gmail.com>
2022-12-06 22:27:35 +0300

    Add styling to home

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,24 +1,37 @@
+import '../css/Home.css';
+
 function Home() {
   return (
-    <div>
-      <h1>Welcome to our Page!</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        sagittis, elit nec convallis semper, sapien lacus commodo sapien, vel
-        pellentesque velit mauris id augue. Morbi a dolor non est euismod
-        scelerisque. Cras vitae sapien eu arcu semper posuere. Mauris tristique
-        augue ipsum, non tempus arcu bibendum et. Etiam nibh libero, dapibus eu
-        rhoncus eu, malesuada id lectus. Etiam porttitor leo sit amet vestibulum
-        pellentesque. Etiam aliquet lorem nec nibh suscipit, sed facilisis arcu
-        condimentum.
-      </p>
-      <p>
-        Nunc faucibus egestas eros, a iaculis arcu rutrum et. Vivamus quis lorem
-        in nulla porttitor gravida. Pellentesque ut rutrum dui. Nam enim ex,
-        dapibus eu risus id, feugiat ultrices enim. Etiam ac condimentum orci.
-        Donec mattis mi ut neque tincidunt eleifend. Morbi tellus ante, cursus
-        ut rutrum vel, feugiat sed orci.
-      </p>
+    <div className="Home-container">
+      <h2 className="Home-Header">Welcome to our Page!</h2>
+      <div className="Home-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          sagittis, elit nec convallis semper, sapien lacus commodo sapien, vel
+          pellentesque velit mauris id augue. Morbi a dolor non est euismod
+          scelerisque. Cras vitae sapien eu arcu semper posuere. Mauris
+          tristique augue ipsum, non tempus arcu bibendum et. Etiam nibh libero,
+          dapibus eu rhoncus eu, malesuada id lectus. Etiam porttitor leo sit
+          amet vestibulum pellentesque. Etiam aliquet lorem nec nibh suscipit,
+          sed facilisis arcu condimentum.
+        </p>
+        <p>
+          Ultricies lacus sed turpis tincidunt id aliquet. Sed viverra ipsum
+          nunc aliquet bibendum enim. Quis hendrerit dolor magna eget est lorem
+          ipsum. Fringilla urna porttitor rhoncus dolor purus. Sit amet
+          venenatis urna cursus eget nunc scelerisque viverra mauris. Purus
+          faucibus ornare suspendisse sed. Faucibus ornare suspendisse sed nisi
+          lacus sed viverra tellus. Quam elementum pulvinar etiam non quam
+          lacus. Fringilla ut morbi tincidunt augue interdum velit euismod. Sit
+          amet nisl purus in mollis. Ipsum faucibus vitae aliquet nec
+          ullamcorper sit amet. Condimentum lacinia quis vel eros donec ac odio.
+          Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras
+          fermentum. Donec et odio pellentesque diam volutpat commodo sed
+          egestas. Quis vel eros donec ac. Consectetur purus ut faucibus
+          pulvinar elementum integer enim neque volutpat. In nisl nisi
+          scelerisque eu ultrices.
+        </p>
+      </div>
     </div>
   );
 }
