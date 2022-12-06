03e7decf94967751c400ace366ade57991c004ce
jellywiz <muzaffer.dler@gmail.com>
2022-12-06 21:23:54 +0300

    Add home file

---
+++ src/component/Home.jsx
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to our Page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        sagittis, elit nec convallis semper, sapien lacus commodo sapien, vel
+        pellentesque velit mauris id augue. Morbi a dolor non est euismod
+        scelerisque. Cras vitae sapien eu arcu semper posuere. Mauris tristique
+        augue ipsum, non tempus arcu bibendum et. Etiam nibh libero, dapibus eu
+        rhoncus eu, malesuada id lectus. Etiam porttitor leo sit amet vestibulum
+        pellentesque. Etiam aliquet lorem nec nibh suscipit, sed facilisis arcu
+        condimentum.
+      </p>
+      <p>
+        Nunc faucibus egestas eros, a iaculis arcu rutrum et. Vivamus quis lorem
+        in nulla porttitor gravida. Pellentesque ut rutrum dui. Nam enim ex,
+        dapibus eu risus id, feugiat ultrices enim. Etiam ac condimentum orci.
+        Donec mattis mi ut neque tincidunt eleifend. Morbi tellus ante, cursus
+        ut rutrum vel, feugiat sed orci.
+      </p>
+    </div>
+  );
+}
+export default Home;
