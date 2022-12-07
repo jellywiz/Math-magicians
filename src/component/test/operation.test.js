1127ab8f0e6fff8090fba3bc0f6221562546209e
jellywiz <muzaffer.dler@gmail.com>
2022-12-07 13:29:12 +0300

    Add multiplication in operation.test.js

--- src/component/test/operation.test.js
+++ src/component/test/operation.test.js
@@ -4,4 +4,8 @@ describe('Testing operation functions', () => {
   test('Addition', () => {
     expect(operate(114, 10, '+')).toBe('124');
   });
+
+  test('Multiplication', () => {
+    expect(operate(44, 3, 'x')).toBe('132');
+  });
 });
